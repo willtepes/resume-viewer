450dff51ce0e3d8b6bdb1a74ecd7ba104a527b46
Will Homan <will.homan@hotmail.com>
2017-04-13 15:53:43 -0700

    website link fixed

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -18,7 +18,7 @@ class Testimonials extends Component {
       <div className="text-container">
          <div className="row">
             <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
+               <h1><span>Inspirational Quotes</span></h1>
             </div>
             <div className="ten columns flex-container">
                 <ul className="slides">
