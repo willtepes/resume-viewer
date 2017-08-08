e088ce5a3eb970c42de2496fc951f3c6e25b4001
William Homan <will.homan@hotmaiml.com>
2017-08-08 15:10:48 -0700

    added carousel line

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -19,6 +19,7 @@ class Portfolio extends Component {
                         <li data-target="#carousel-portfolio" data-slide-to="1"></li>
                         <li data-target="#carousel-portfolio" data-slide-to="2"></li>
                         <li data-target="#carousel-portfolio" data-slide-to="3"></li>
+                        <li data-target="#carousel-portfolio" data-slide-to="4"></li>
                         </ol>
 
                         {/* Wrapper for slides */}
